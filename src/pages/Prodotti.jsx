d4d0d0e96a56f68bc3db7f8f1e3f9c9e9c6d2ebf
BkStyle89 <kajzer@hotmail.it>
2026-03-20 15:31:03 +0100

    products map

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -6,20 +6,35 @@ export default function Prodotti(){
 const api_link ="https://fakestoreapi.com/products"
 const [products,setProducts]=useState([])
 
-/* function getProducts(){
+function getProducts(){
 fetch(api_link)
 .then(res=res.json())
 .then(data=>{setProducts(data)
 
 })
 }
-useEffect(getProducts,[]) */
+useEffect(getProducts,[]) 
 
 return(
     <div>
         <AppHeader/>
         <main>
-            
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+                    {products.map(product=>(
+                        <div key={product.id}>
+                            <div className="col">
+                                <h3>{product.title}</h3>
+                                <p>{product.price}</p>
+                                <p>{product.description}</p>
+                                <p>{product.category}</p>
+                                <img src={product.image} alt="" />
+                                <h5>{product.rating}</h5>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>  
         </main>
         <AppFooter/>
     </div>
