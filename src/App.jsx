d44de75ee879740bf7f0795fd5d96cdab1c03784
BkStyle89 <kajzer@hotmail.it>
2026-03-20 12:53:31 +0100

    trying to do the nav link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import ChiSiamo from "./pages/ChiSiamo"
+import Prodotti from "./pages/Prodotti"
 function App() {
  
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <nav>
+            <NavLink to="/" element={<Homepage/>}/>
+            <NavLink to="/ChiSiamo" element={<ChiSiamo/>}/>
+            <NavLink to="Prodotti" element={<Prodotti/>}/>
+          </nav>  
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
