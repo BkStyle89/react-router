9f0233ffdc8aadd27df49775e7f3608aa7abc413
BkStyle89 <kajzer@hotmail.it>
2026-03-20 15:23:10 +0100

    boostrap installed

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,19 +1,19 @@
 import AppHeader from "../components/AppHeader"
 import AppFooter from "../components/AppFooter"
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 export default function Prodotti(){
 
 const api_link ="https://fakestoreapi.com/products"
 const [products,setProducts]=useState([])
 
-function getProducts(){
+/* function getProducts(){
 fetch(api_link)
 .then(res=res.json())
 .then(data=>{setProducts(data)
 
 })
 }
-useEffect(getProducts,[])
+useEffect(getProducts,[]) */
 
 return(
     <div>
