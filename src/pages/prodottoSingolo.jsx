acc7d60548af28afc3e4a19691a110da91f81c29
BkStyle89 <kajzer@hotmail.it>
2026-03-23 12:22:56 +0100

    fetch connection

--- src/pages/prodottoSingolo.jsx
+++ src/pages/prodottoSingolo.jsx
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router";
 import AppHeader from "../components/AppHeader"
 import AppFooter from "../components/AppFooter"
 
 export default function prodottoSingolo(){
 const {id} = useParams();
+const navigate =useNavigate
     useEffect(()=>{
-
-    },[]);
+        fetch("https://fakestoreapi.com/products${id}")
+        .then(res=>{
+            console.log(res.data);
+        })
+        .catch(err=>{
+            navigate(-1);
+        });
+    },[id,navigate]);
     return (
         <div>
             <AppHeader/>
